4e2084da90e95a6d44441986d27d27c90085cade
Mark Helt <heltmm@gmail.com>
2018-01-07 12:40:55 -0800

    config allowed websocket connections

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,9 @@ Rails.application.configure do
   # Set heruku web socket url
   config.web_socket_server_url = "wss://https://weather-station-.herokuapp.com/cable"
 
+  # Set Websocket request Origin
+  config.action_cable.allowed_request_origins = ['https://iot-dash-board.herokuapp.com/', 'http://iot-dash-board.herokuapp.com/']
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
